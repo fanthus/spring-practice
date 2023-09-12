c5a9166a7d1eae088458bf71173da2b28036f20c
fanxiushan <fanxiushan@gmail.com>
2023-09-12 20:00:37 +0800

    feat: 添加/hello接口

---
+++ src/main/java/com/fanthus/springpractice/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.fanthus.springpractice.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
