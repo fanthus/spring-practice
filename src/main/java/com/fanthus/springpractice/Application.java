ff32f763fa1b4e0802e5db028323078d62662e10
fanxiushan <fanxiushan@gmail.com>
2023-09-12 19:49:42 +0800

    feat: 服务器配置初始化

--- src/main/java/com/fanthus/springpractice/Application.java
+++ src/main/java/com/fanthus/springpractice/Application.java
@@ -1,7 +1,11 @@
 package com.fanthus.springpractice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        SpringApplication.run(Application.class, args);
     }
 }
