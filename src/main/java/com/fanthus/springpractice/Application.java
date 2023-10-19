4bfc2267e5c9ae658dcd16614f49045f3ef9c0df
fanxiushan <fanxiushan@gmail.com>
2023-10-19 20:29:56 +0800

    feat: 修改端口

--- src/main/java/com/fanthus/springpractice/Application.java
+++ src/main/java/com/fanthus/springpractice/Application.java
@@ -3,9 +3,14 @@ package com.fanthus.springpractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication()
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
     }
 }
