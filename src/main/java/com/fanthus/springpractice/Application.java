309ab926a87668c54ed40726c4eb4dfae12086d6
fanxiushan <fanxiushan@gmail.com>
2023-10-24 11:25:27 +0800

    添加属性文件

--- src/main/java/com/fanthus/springpractice/Application.java
+++ src/main/java/com/fanthus/springpractice/Application.java
@@ -1,15 +1,19 @@
 package com.fanthus.springpractice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Collections;
 
 @SpringBootApplication()
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        //在 Application.properties 修改了
+//        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
         app.run(args);
     }
 }
---
+++ src/main/java/com/fanthus/springpractice/controller/PropertyController.java
@@ -0,0 +1,27 @@
+package com.fanthus.springpractice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class PropertyController {
+
+    @Value("${person.name}")
+    public String name;
+
+    @Value("${person.score:100}")   //没有提供这个属性则使用 100 作为默认值
+    public String score;
+
+    @GetMapping("/get/property/name")
+    public String getPropertyName() {
+        return "getProperty name : "+name;
+    }
+
+    @GetMapping("/get/property/score")
+    public String getPropertyScore() {
+        return "getProperty score : "+score;
+    }
+}
